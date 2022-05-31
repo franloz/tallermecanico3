a24c983725b67587190e5e6b1d71ccc534c5edcb
fran <francislozgut@gmail.com>
2022-05-31 18:37:37 +0200

    y

--- lib/controller/clientController.dart
+++ lib/controller/clientController.dart
@@ -5,10 +5,18 @@ import 'package:tallermecanico/alertdialog/dialogError.dart';
 import 'package:tallermecanico/databases/database.dart';
 import 'package:tallermecanico/model/client.dart';
 
+
+
+
 class ClientController {
+
+  
+  
+
+  //cliente
   Future<void> insertClient(BuildContext context, Client client) async {
     DatabaseSqlite db = DatabaseSqlite();
-    Database database = await db.openDB();
+  Database database = await db.openDB();
 
     try {
       await database.insert("Clientes", client.toMap());
@@ -19,32 +27,30 @@ class ClientController {
     }
   }
 
-  Future<void> deleteClient(BuildContext context, String dni) async {
+  Future<void> deleteClient(BuildContext context,String dni) async {
     DatabaseSqlite db = DatabaseSqlite();
-    Database database = await db.openDB();
+  Database database = await db.openDB();
 
     //esto es para saber si el cliente que se quiere borrar tiene vehiculos, si las tiene no le dejará borrar al cliente hasta que borre las vehiculos
-    final List<Map<String, dynamic>> maps = await database
-        .rawQuery('SELECT * FROM Vehiculos WHERE clientedni = ?', [dni]);
-    int count = maps
-        .length; //si dejase borrar a los clientes y luego pulsase sobre modificar en un vehiculo que contenia a ese cliente daria error el combobox porque ese cliente ya no existe
-    print('jjjj' + count.toString());
+    final List<Map<String, dynamic>> maps = await database.rawQuery('SELECT * FROM Vehiculos WHERE clientedni = ?', [dni]);
+    int count=   maps.length;//si dejase borrar a los clientes y luego pulsase sobre modificar en un vehiculo que contenia a ese cliente daria error el combobox porque ese cliente ya no existe
+    print('jjjj'+count.toString());
 
-    if (count == 0) {
+    if(count==0){
       await database.delete("Clientes", where: 'dni = ?', whereArgs: [dni]);
-      print('bbbbb' + 'borrado');
-    } else {
-      String error =
-          'Este cliente no se puede borrar debido a que existen vehículos con este cliente, deberá borrarlos antes de poder borrar al cliente';
+      print('bbbbb'+'borrado');
+    }else{
+      String error = 'Este cliente no se puede borrar debido a que existen vehículos con este cliente, deberá borrarlos antes de poder borrar al cliente';
       DialogError dialogError = DialogError();
       await dialogError.dialogError(context, error);
     }
+
   }
 
   Future<void> updateClient(
       BuildContext context, Client client, String dni) async {
     DatabaseSqlite db = DatabaseSqlite();
-    Database database = await db.openDB();
+  Database database = await db.openDB();
 
     try {
       await database.update("Clientes", client.toMap(),
@@ -57,8 +63,8 @@ class ClientController {
   }
 
   Future<List<Client>> getClients() async {
-    DatabaseSqlite db = DatabaseSqlite();
-    Database database = await db.openDB();
+   DatabaseSqlite db = DatabaseSqlite();
+  Database database = await db.openDB();
 
     final List<Map<String, dynamic>> maps = await database.query('Clientes');
 
@@ -73,9 +79,11 @@ class ClientController {
     });
   }
 
+ 
+
   Future<List<Client>> getClientsWhere(String nombre) async {
     DatabaseSqlite db = DatabaseSqlite();
-    Database database = await db.openDB();
+  Database database = await db.openDB();
 
     final List<Map<String, dynamic>> maps = await database
         .rawQuery('SELECT * FROM Clientes WHERE nombre LIKE ?', [nombre + '%']);
@@ -90,4 +98,9 @@ class ClientController {
       );
     });
   }
+
+  //cliente
+
+
+
 }
