6b4e273202adadaf52817647929073f857a180d7
fran <francislozgut@gmail.com>
2022-06-01 13:35:13 +0200

    lkjm

--- lib/pdf/pdf_api.dart
+++ lib/pdf/pdf_api.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart';
 
 
@@ -49,7 +50,7 @@ class PdfApi{
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Factura')
+            Text('Factura',style: TextStyle(fontSize:24,fontWeight:FontWeight.bold,color:PdfColors.black))
 
 
         ]),
